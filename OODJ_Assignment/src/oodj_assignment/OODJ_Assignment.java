6a6a246d7236f9946f1ac5d41997f854fd58f62d
JONATHAN OWEN PANG <TP060491@mail.apu.edu.my>
2021-07-17 22:38:57 +0800

    Clean up things ?

--- OODJ_Assignment/src/oodj_assignment/OODJ_Assignment.java
+++ OODJ_Assignment/src/oodj_assignment/OODJ_Assignment.java
@@ -2,15 +2,15 @@
 package oodj_assignment;
 
 
-
-
-
-
-
+    
+    
+    
+    
+    
 
 public class OODJ_Assignment {
     public static void main(String[] args) {
-        
+
         //testing 123
         //ToPush
         
