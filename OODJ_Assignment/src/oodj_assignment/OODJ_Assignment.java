121a518a5634fcd5b321dd9d4a009fa22d2266bc
GGene-E <eugenetyeweechin@gmail.com>
2021-07-17 22:30:12 +0800

    Update 1 (person, admin, cust)
    Created Person, Admin, Customer class
    
    Created attributes, getters, setters for Person
    
    Created Customer and Admin class that inherits Person

--- OODJ_Assignment/src/oodj_assignment/OODJ_Assignment.java
+++ OODJ_Assignment/src/oodj_assignment/OODJ_Assignment.java
@@ -1,18 +1,12 @@
 
 package oodj_assignment;
 
-
-
-
-
-
-
-
 public class OODJ_Assignment {
     public static void main(String[] args) {
         
         //testing 123
         //ToPush
+        //testing again
         
     }
     
