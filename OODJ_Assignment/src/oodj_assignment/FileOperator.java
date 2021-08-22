8e1ea2b010d94c1e6e2d6daaac2daf658a389c27
Kiku-Ichimonji <jonathan.op.2000@gmail.com>
2021-08-22 15:30:57 +0800

    Added AppendMode

--- OODJ_Assignment/src/oodj_assignment/FileOperator.java
+++ OODJ_Assignment/src/oodj_assignment/FileOperator.java
@@ -5,8 +5,18 @@ import java.io.*;
 import java.util.ArrayList;
 
 
-public class FileOperator {
+public class FileOperator extends ObjectOutputStream{
     
+    public FileOperator(OutputStream output) throws IOException //Run this to Append
+    {
+        super(output);
+    }
+    
+    @Override
+    protected void writeStreamHeader() throws IOException
+    {
+        reset();
+    }
     
     public void addProduct()
     {
@@ -58,6 +68,36 @@ public class FileOperator {
         return null;
     }        
         
+    public Boolean writeCustomers(Customer customerObject)
+    {
+        if(compareSimilarities(customerObject) == true)//JONATHAN - Update compareSimilarities
+        {
+            return false; //There are similarities
+        }
+        
+        File personFile = new File("person.txt");
+        try
+        {
+            FileOutputStream fileOutputStream = new FileOutputStream(personFile);
+            try
+            {
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(customerObject);
+            }
+            catch(IOException ex)
+            {
+                
+            }
+        }
+        catch(FileNotFoundException ex)
+        {
+            
+        }
+        
+        
+        return true;
+    }
+    
     public Boolean compareSimilarities(Person personObject)
     {
         if(personObject.getUsername().equals("A"))
